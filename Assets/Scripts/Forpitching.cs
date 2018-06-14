a89ab4a5dec1acd276ff057079cb350a21e4a986
Thanawat <Spirit_windwalk@hotmail.com>
2018-06-15 00:12:33 +0700

    Slime-Atk

--- Assets/Scripts/Forpitching.cs
+++ Assets/Scripts/Forpitching.cs
@@ -6,17 +6,30 @@ public class Forpitching : MonoBehaviour {
 
     public float speed;
     private Transform targetplayer;
-	// Use this for initialization
-	void Start () {
+    private Animator animator;
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    // Use this for initialization
+    void Start () {
         targetplayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if(Vector2.Distance(transform.position, targetplayer.position)> 1)
+        if(Vector2.Distance(transform.position, targetplayer.position) > 1)
         {
+            animator.SetInteger("SlimeState", 0);
             transform.position = Vector2.MoveTowards(transform.position, targetplayer.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetInteger("SlimeState", 1);
+
         }
     }
 }
