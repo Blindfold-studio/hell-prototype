757faa48ae19a9844f45578fcf36aed502a9d809
RocKIn0X <dantai.l@ku.th>
2018-06-25 11:39:27 +0300

    add improve jump

---
+++ Assets/Scripts/ImproveJump.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImproveJump : MonoBehaviour {
+    [SerializeField]
+    private float fallMul = 2.5f;
+    [SerializeField]
+    private float lowJumpMul = 2f;
+
+    private Rigidbody2D rb2d;
+
+	void Awake () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rb2d.velocity.y < 0)
+        {
+            rb2d.gravityScale = fallMul;
+        }
+        else if (rb2d.velocity.y > 0 && !Input.GetKey(KeyCode.Z)) 
+        {
+            rb2d.gravityScale = lowJumpMul;
+        }
+        else
+        {
+            rb2d.gravityScale = 1f;
+        }
+	}
+}
