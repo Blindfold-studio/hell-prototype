cbfb23127ba0eb0fce4eddd8cb0dad50c59926ac
Piromsurang Rungserichai <piromsurang@Piromsurang.local>
2018-06-26 15:19:00 +0300

    item effects

---
+++ Assets/Scripts/Gift/PlayerBoost.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBoost : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.name == "Player1") {
+            GameObject g = GameObject.Find("Player2");
+            Vector2 v = g.transform.localScale;
+            v += new Vector2(1f, 1f);
+            g.transform.localScale = v;
+            this.gameObject.SetActive(false);
+        } else if(other.gameObject.name == "Player2") {
+            GameObject g = GameObject.Find("Player1");
+
+        }
+
+    }
+
+}
