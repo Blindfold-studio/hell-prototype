91d9fd6d88520f037bec0e32ad1a06c510ba4333
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2018-06-26 15:40:26 +0300

    try to use powerboost code

--- Assets/Scripts/Gift/PlayerBoost.cs
+++ Assets/Scripts/Gift/PlayerBoost.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class PlayerBoost : MonoBehaviour {
+    private GameObject g;
 
 	private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.name == "Player1") {
-            GameObject g = GameObject.Find("Player2");
-            Vector2 v = g.transform.localScale;
-            v += new Vector2(1f, 1f);
-            g.transform.localScale = v;
-            this.gameObject.SetActive(false);
+            g = GameObject.Find("Player2");
+            
         } else if(other.gameObject.name == "Player2") {
-            GameObject g = GameObject.Find("Player1");
+            g = GameObject.Find("Player1");
 
         }
-
+        Vector2 v = g.transform.localScale;
+        v += new Vector2(1f, 1f);
+        g.transform.localScale = v;
+        this.gameObject.SetActive(false);
     }
 
 }
